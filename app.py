a0042bb0a2d282d1704259d91944e6760559c787
suyash8848 <61310436+suyash8848@users.noreply.github.com>
2023-09-12 10:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
 from flask import Flask, render_template, request, redirect, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-import os
 import psycopg2
+import webbrowser
 
-app = Flask(__name__, static_folder='static')
-
-if not 'RUNNING_IN_PRODUCTION' in os.environ:
-   # Local development, where we'll use environment variables.
-   print("Loading config.development and environment variables from .env file.")
-   app.config.from_object('azureproject.development')
-else:
-   # Production, we don't load environment variables from .env file but add them as environment variables in Azure.
-   print("Loading config.production.")
-   app.config.from_object('azureproject.production')
-
-with app.app_context():
-    app.config.update(
-        SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Suyash12345@localhost/suyash"
+app = Flask(__name__)
+app.secret_key = 'my_secret_key' 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Flask_App_23@postgres-db-50.postgres.database.azure.com/employee_data"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -83,4 +67,5 @@ def about():
 
 
 if __name__ == "__main__":
+    webbrowser.open('http://localhost:5000/')
     app.run()
