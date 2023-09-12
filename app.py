c68106e81236ee24ccb528935e4dafa42aca50d0
suyash8848 <61310436+suyash8848@users.noreply.github.com>
2023-09-12 20:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 app.secret_key = '4552702f4c7988ede67653b38c73db3c6ccaf8db0092b65bd56c374a1f6608da' 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://emp2admin:Flask_App_23@emp-app-2.postgres.database.azure.com/employee_data"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://zboweaunuh:074517G43AL0EO47$@empapp1-server.postgres.database.azure.com/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
